42aa0ff32c20a67f188d841f699af08d99f678c1
Mitchell Reece <mitchellreece@croud.co.uk>
2017-07-06 13:52:05 +0100

    component example added

--- src/components/shared/Paginator.vue
+++ src/components/shared/Paginator.vue
@@ -1,22 +1,67 @@
 <template>
-    <h1>{{ test }}</h1>
+    <div class="ui small pagination menu" v-if="pagination.total_pages > 1 && pagination.total_pages <= 5">
+        <span v-for="page in pages">
+              <a class="item" :class="{active: current === page}" @click="current = page">{{ page }}</a>
+        </span>
+    </div>
+
+    <div class="ui small pagination borderless menu" v-else-if="pagination.total_pages > 1">
+        <a :class="startClass" @click="current = 1">First Page</a>
+        <a :class="startClass" @click="current -= 1">Prev</a>
+        <a class="item">Page {{current}} of {{pagination.total_pages}}</a>
+        <a :class="endClass" class="item" @click="current += 1">Next</a>
+        <a :class="endClass" @click="current = pagination.total_pages">Last Page</a>
+    </div>
 </template>
 
 <script>
     /**
-     * This is the croud paginator.
-     * @example ./croud-paginator.md
-     */
+    * @example ./croud-paginator.md
+    */
     export default {
         name: 'croud-paginator',
-
+        model: {
+            prop: 'currentPage',
+        },
         props: {
             /**
-             * The color for the button.
-             */
-            test: {
-                type: String,
-                default: 'test',
+            *
+            */
+            pagination: {
+                required: true,
+                type: Object,
+            },
+            /**
+            * Page Currently being displayed
+            */
+            currentPage: {
+                type: Number,
+                required: true,
+            },
+        },
+
+        computed: {
+            current: {
+                get() {
+                    return this.currentPage
+                },
+                set(page) {
+                    this.$emit('set-current', page)
+                    this.$emit('input', page)
+                },
+            },
+
+            pages() {
+                /* eslint-disable prefer-spread */
+                return Array.apply(null, { length: this.pagination.total_pages }).map((v, k) => k + 1)
+            },
+
+            startClass() {
+                return this.current > 1 ? 'item' : 'item disabled'
+            },
+
+            endClass() {
+                return this.current < this.pagination.total_pages ? 'item' : 'item disabled'
             },
         },
     }
--- src/components/shared/croud-paginator.md
+++ src/components/shared/croud-paginator.md
@@ -1,3 +1,5 @@
 ## Simple Paginator
+    <croud-paginator :pagination="{total_pages: 5}" :currentPage="1"></croud-paginator>
 
-    <croud-paginator test="another"></croud-paginator>
+## Larger Paginator
+    <croud-paginator :pagination="{total_pages: 12}" :currentPage="5"></croud-paginator>
