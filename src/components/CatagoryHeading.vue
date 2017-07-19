45af41754f7201e2db04faeb5ba56b940285ad0e
Mitchell Reece <mitchellreece@croud.co.uk>
2017-07-19 07:22:29 +0100

    Add new navigation

---
+++ src/components/CatagoryHeading.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="catagory">
+        <header class="catagory__heading">
+            <h1 class="catagory__title">{{catagory}}</h1>
+        </header>
+        <main>
+            <router-view></router-view>
+        </main>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        catagory: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+    @import '../resources/sass/variables/_all.scss';
+    .catagory{
+        &__heading{
+            position: fixed;
+            background-color: $croud-colour-white;
+            width: 100%;
+            z-index: 100;
+            top:0;
+            left: 15rem;
+        }
+        &__title{
+            font-size: $croud-font-size-huge;
+            font-family: $croud-font-body;
+            text-transform: uppercase;
+            padding: 3rem 1em;
+            color: rgba($croud-colour-black, .3);
+        }
+    }
+</style>
