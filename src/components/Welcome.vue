60b538ec90bb664597b56f99eb2497828f01d682
Mitchell Reece <mitchellreece@croud.co.uk>
2017-07-19 07:22:29 +0100

    Add new navigation

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,5 @@
 <template lang="html">
   <div>
-      <h1 class="ui croud header">Croud Style Guide</h1>
-      <h2 class="ui header">Welcome!</h2>
       <p>The Croud Style Guide provides guidelines on how our applications should look, it is based on our company branding at present.</br> It shows an example of semantic ui elements in the style we should use them.</p>
       <p>The style guide also incorporates the Vue-Semantic Showcase, so you can quickly grab Vue-Semantic component code to use in your applications.</p>
   </div>
