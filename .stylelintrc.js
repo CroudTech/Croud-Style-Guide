4f7aea93de37e210c0daa72d5894e0c05ec98eb6
Mitchell Reece <mitchellreece@croud.co.uk>
2017-07-26 11:25:56 +0100

    Setup variable checker plugin

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     extends: ['stylelint-config-recommended'],
     processors: ['@mapbox/stylelint-processor-arbitrary-tags'],
+    plugins: ['stylelint-declaration-use-variable'],
     rules: {
         'no-empty-source': null,
-    }
+        "sh-waqar/declaration-use-variable": [
+            'font-family',
+        ],
+    },
 }
