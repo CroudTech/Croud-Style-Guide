857d9fee2e6f9c7e952c08e9d4a84809898e6f9c
Brock <brock.reece@croud.co.uk>
2017-07-27 17:03:48 +0100

    Fix grammerly issue .... grrr

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -50,7 +50,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-quill/src/formats/')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
