be5a0e98f4d3eadc6d5565dd21813412ef4dc6d2
Mitchell Reece <mitchellreece@croud.co.uk>
2017-07-27 13:37:06 +0100

    Add checkbox component

---
+++ src/components/shared/forms/Checkbox.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="checkbox">
+        <input :id="id" :name="name" type="checkbox" :checked="checked"/>
+        <label :for="id" class="checkbox__box">
+        <i class="icon check"></i>
+        </label>
+  </div>
+</template>
+
+<script>
+/**
+ * Basic circular checkbox component
+ * @example ./croud-checkbox.md
+*/
+export default {
+    name: 'croud-checkbox',
+    props: {
+        /**
+         * use to default to checked
+         */
+        checked: {
+            type: Boolean,
+            default: false,
+        },
+        /**
+         * id for checkbox
+         */
+        id: {
+            type: String,
+            required: true,
+        },
+        /**
+         * name for checkbox
+         */
+        name: {
+            type: String,
+            default: '',
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../../../resources/sass/variables/all.scss';
+
+    input[type=checkbox] {
+        display: none;
+    }
+
+    .checkbox {
+        height: 1.3em;
+        width: 1.3em;
+        font-size: $croud-font-size-medium;
+        position: relative;
+        display: inline-block;
+    
+        &::after {
+            content: '';
+            display: block;
+            clear: both;
+        }
+
+        &__box {
+            top: 0;
+            left: 0;
+            position: absolute;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+            border-radius: 50%;
+            border: .1em solid rgba(#8e979b, .7);
+            box-sizing: border-box;
+            transition: all $croud-motion-duration-slow $croud-motion-ease-sharp;
+
+            &:hover{
+                border-color: #8e979b;
+            }
+
+            .icon {
+                color: #fff;
+                font-size: $croud-font-size-mini;
+                line-height: 1.2;
+                margin-right: 0;
+                display: none;
+            }
+        }
+}
+
+.primary{
+    &.checkbox input[type=checkbox]:checked + .checkbox__box{
+        border-color: $croud-colour-primary;
+        background-color: $croud-colour-primary;
+    }
+}
+
+.success{
+    &.checkbox input[type=checkbox]:checked + .checkbox__box{
+        border-color: $croud-colour-success;
+        background-color: $croud-colour-success;
+    }
+}
+
+.checkbox input[type=checkbox]:checked + .checkbox__box{
+    border-color: $croud-colour-info;
+    background-color: $croud-colour-info;
+
+   .check {
+     display: inline;
+   }
+}
+</style>
---
+++ src/components/shared/forms/croud-checkbox.md
@@ -0,0 +1,19 @@
+### Default Checkbox
+  
+    <croud-checkbox name="showThis" id="input_one" />
+
+### Checked by Default
+Pass through the checked prop to check the checkbox by default
+
+    <croud-checkbox name="showThat" id="input_two" checked />
+
+### Style Variations
+You can also change the style of the checkbox by assigning the following classes:
+
+**primary:**
+
+    <croud-checkbox name="primary" id="input_three" class="primary" checked />
+
+**success:**
+
+    <croud-checkbox name="success" id="input_four" class="success" checked />
