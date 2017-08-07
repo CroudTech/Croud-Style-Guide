cd8b2efc694144a487ea7146f472b7a8497dc45b
Mitchell Reece <mitchellreece@croud.co.uk>
2017-08-07 16:07:59 +0100

    setup jquery with jest test env

--- config/test.env.js
+++ config/test.env.js
@@ -1,5 +1,8 @@
 var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
+var $ = require('jquery')
+
+global.$ = global.jQuery = $;
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"'
