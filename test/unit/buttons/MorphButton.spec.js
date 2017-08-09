79534f044cd163ba6b94e0a361ac8fe5bb813971
Mitchell Reece <mitchellreece@croud.co.uk>
2017-08-09 12:03:16 +0100

    test morph button options method calls

--- test/unit/buttons/MorphButton.spec.js
+++ test/unit/buttons/MorphButton.spec.js
@@ -6,8 +6,8 @@ const Constructor = Vue.extend(MorphButton)
 const vm = new Constructor({
     propsData: {
         options: [
-            { name: 'close all' },
-            { name: 'expand all' },
+            { name: 'close all', method: jest.fn() },
+            { name: 'expand all', method: jest.fn() },
         ],
         cta: true,
         active: 1,
@@ -27,10 +27,19 @@ describe('Morph Button', () => {
         expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.activeIndex].name)
     })
 
-    it('should toggle text on click', () => {
-        vm.$el.click()
-        expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
-        vm.$el.click()
-        expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
+    describe('Click Event', () => {
+        it('should call correct method', () => {
+            vm.$el.click()
+            expect(vm.options[1 - vm.active].method).toHaveBeenCalled()
+            vm.$el.click()
+            expect(vm.options[1 - vm.active].method).toHaveBeenCalled()
+        })
+
+        it('should toggle button text', () => {
+            vm.$el.click()
+            expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
+            vm.$el.click()
+            expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
+        })
     })
 })
