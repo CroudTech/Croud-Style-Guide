c0f22a1b810ac2f987c5112acd28b0a0cf60c666
Mitchell Reece <mitchellreece@croud.co.uk>
2017-08-09 11:25:40 +0100

    test morph button functionality

--- test/unit/buttons/MorphButton.spec.js
+++ test/unit/buttons/MorphButton.spec.js
@@ -6,15 +6,31 @@ const Constructor = Vue.extend(MorphButton)
 const vm = new Constructor({
     propsData: {
         options: [
-            'expand all',
-            'close all',
+            { name: 'close all' },
+            { name: 'expand all' },
         ],
         cta: true,
+        active: 1,
     },
 }).$mount()
 
 describe('Morph Button', () => {
     it('should match the snapshot', () => {
         expect(vm.$el).toMatchSnapshot()
+    })
+
+    it('should override active index when active prop is passed', () => {
+        expect(vm.activeIndex).toBe(vm.active)
+    })
+
+    it('should display the correct text on render', () => {
+        expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.activeIndex].name)
+    })
+
+    it('should toggle text on click', () => {
+        vm.$el.click()
+        expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
+        vm.$el.click()
+        expect(vm.$el.innerHTML).toBe(vm.options[1 - vm.active].name)
     })
 })
