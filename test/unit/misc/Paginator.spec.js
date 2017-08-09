4e88452fb87d0405f14e2adfbc517f14df3659f8
Mitchell Reece <mitchellreece@croud.co.uk>
2017-08-09 15:05:37 +0100

    test paginator render

--- test/unit/misc/Paginator.spec.js
+++ test/unit/misc/Paginator.spec.js
@@ -16,4 +16,38 @@ describe('Paginator', () => {
     it('should match the snapshot', () => {
         expect(vm.$el).toMatchSnapshot()
     })
+
+    describe('Above 5 Pages', () => {
+        beforeEach(() => {
+            vm.pagination.total_pages = 6
+        })
+
+        it('should only render large paginator', () => {
+            expect(vm.$el.querySelectorAll('span').length).toBe(0)
+        })
+    })
+
+    describe('Less Than 5 Pages', () => {
+        beforeEach(() => {
+            vm.pagination.total_pages = 3
+        })
+
+        it('should only render small paginator', () => {
+            expect(vm.$el.querySelectorAll('span').length).not.toBe(0)
+        })
+
+        it('should render all page items', () => {
+            expect(vm.$el.querySelectorAll('.item').length).toBe(vm.pagination.total_pages)
+        })
+    })
+
+    describe('Only Single Page', () => {
+        beforeEach(() => {
+            vm.pagination.total_pages = 1
+        })
+
+        it('shouldn\'t render', () => {
+            expect(vm.$el.childElementCount).toBeUndefined()
+        })
+    })
 })
