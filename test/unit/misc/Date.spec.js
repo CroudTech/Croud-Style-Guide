d584d5811f83868f53638a60891ef1a369bae546
Mitchell Reece <mitchellreece@croud.co.uk>
2017-08-09 12:45:32 +0100

    test date functionality

--- test/unit/misc/Date.spec.js
+++ test/unit/misc/Date.spec.js
@@ -7,11 +7,16 @@ const Constructor = Vue.extend(Date)
 const vm = new Constructor({
     propsData: {
         date: moment('2017-08-08T14:20:04+00:00'),
+        format: 'll',
     },
 }).$mount()
 
 describe('Date', () => {
     it('should match the snapshot', () => {
         expect(vm.$el).toMatchSnapshot()
+    })
+
+    it('should format date as specified', () => {
+        expect(vm.$el.innerHTML).toBe(vm.date.format(vm.format))
     })
 })
