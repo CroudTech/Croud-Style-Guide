ff1900e67faf74f0146701e9910490909635e5ab
Brock <brock.reece@croud.co.uk>
2017-11-10 08:37:25 +0000

    Fix tests

--- test/unit/forms/SearchDropdown.spec.js
+++ test/unit/forms/SearchDropdown.spec.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import VueSemantic from 'croud-vue-semantic'
 import SearchDropdown from '../../../src/components/shared/forms/SearchDropdown'
 import '../../../semantic/dist/semantic'
 
-Vue.use(require('croud-vue-semantic'))
+Vue.use(VueSemantic)
 
 const Constructor = Vue.extend(SearchDropdown)
 
