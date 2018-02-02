8890c47691eb90e9f2cb3a4529f8ab3ba04f86ff
Brock <brock.reece@croud.co.uk>
2018-02-02 11:54:27 +0000

    Update filterbar styling

--- src/components/shared/layout/Filterbar.vue
+++ src/components/shared/layout/Filterbar.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="ui secondary basic segment">
         <div class="ui text menu">
-            <div class="item">
-                <div class="ui icon input">
-                    <i class="search icon"></i>
-                    <input  placeholder="Search..." v-model="_search"/>
+            <div class="left item">
+                <div class="ui compact basic segment">
+                    <strong>Search</strong>
+                    <div class="ui text menu">
+                        <div class="item">
+                            <div class="ui icon input">
+                                <i class="search icon"></i>
+                                <input :placeholder="placeholder" v-model="_search"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="right item">
@@ -31,6 +38,14 @@
                 type: String,
                 default: '',
             },
+
+           /**
+            * Placeholder text
+            */
+            placeholder: {
+                type: String,
+                default: '',
+            },
         },
 
         computed: {
@@ -50,6 +65,21 @@
     .ui.secondary.basic.segment {
         padding: 0.5em 0 0;
         margin: 0;
+
+        .item.left {
+            padding: 0 1em 1em;
+            margin: 0;
+
+            .segment {
+                padding: 0;
+                margin: 0;
+            }
+
+            .ui.text.menu .item {
+                margin: 0;
+                padding: 0;
+            }
+        }
 
         .right.item {
             padding: 0;
