c9ecbc098077bfb3b28b2bb920fcba9f92a082ad
Antony King <antony.king@croud.co.uk>
2018-03-02 09:55:55 +0000

    update tests

--- test/unit/layout/Panel.spec.js
+++ test/unit/layout/Panel.spec.js
@@ -3,10 +3,39 @@ import Panel from '../../../src/components/shared/layout/Panel'
 
 const Constructor = Vue.extend(Panel)
 
-const vm = new Constructor().$mount()
+// const vm = new Constructor().$mount()
+
+
+const vm = new Constructor({
+    propsData: {
+        title: 'Test Panel',
+        showCollapseToggle: true,
+    },
+}).$mount()
 
 describe('Panel', () => {
     it('should match the snapshot', () => {
         expect(vm.$el).toMatchSnapshot()
+    })
+})
+
+describe('showCollapseToggle is false', () => {
+    beforeEach(() => {
+        vm.showCollapseToggle = false
+    })
+
+    it('shouldnt show the show / collapse button', () => {
+        expect(vm.$el.querySelectorAll('.circular .angle .icon').length).toBe(0)
+    })
+})
+
+describe('No title length and showCollapseToggle is false', () => {
+    beforeEach(() => {
+        vm.title = ''
+        vm.showCollapseToggle = false
+    })
+
+    it('shouldnt show the text segment', () => {
+        expect(vm.$el.querySelectorAll('.text').length).toBe(0)
     })
 })
