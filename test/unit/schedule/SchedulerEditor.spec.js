73d3fd045452749509876f971ec196eb6f0f4319
Brock <brock.reece@croud.co.uk>
2018-03-09 08:54:34 +0000

    Add basic structure to tests, fix empty root object and fix readonly attr in snapshot

--- test/unit/schedule/SchedulerEditor.spec.js
+++ test/unit/schedule/SchedulerEditor.spec.js
@@ -10,44 +10,27 @@ const Constructor = Vue.extend(SchedulerEditor)
 
 const vm = new Constructor({
     propsData: {
-        schedule: {
-            months: {
-                january: false,
-                february: false,
-                march: false,
-                april: false,
-                may: false,
-                june: false,
-                july: false,
-                august: false,
-                september: false,
-                october: false,
-                november: false,
-                december: false,
-            },
-            days: {
-                monday: false,
-                tuesday: false,
-                wednesday: false,
-                thursday: false,
-                friday: false,
-                saturday: false,
-                sunday: false,
-            },
-            recur: 'daily',
-            at: '00:00',
-            timezone: ' Europe/London',
-            limit: {
-                from: moment().format('YYYY-MM-DD hh:mm:ss'),
-                to: '',
-                count: null,
-            },
+        rootObject: {
+            id: 1,
+            name: 'rootObject',
         },
     },
 }).$mount()
 
 describe('Scheduler Editor', () => {
-    it('should match the snapshot', () => {
-        expect(vm.$el).toMatchSnapshot()
+    it('should match the snapshot', (done) => {
+        vm.$nextTick(() => {
+            expect(vm.$el).toMatchSnapshot()
+            done()
+        })
+    })
+
+
+    describe('data transformation', () => {
+        describe('input', () => {
+        })
+
+        describe('output', () => {
+        })
     })
 })
