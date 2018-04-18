13e4c109353127908392648cf97a411d2027d158
Brock <brock.reece@croud.co.uk>
2018-04-18 14:50:36 +0100

    Use disabled prop to disable button

--- src/components/shared/buttons/SaveButton.vue
+++ src/components/shared/buttons/SaveButton.vue
@@ -19,7 +19,7 @@
                     loading: context.props.loading,
                 },
                 attrs: {
-                    disabled: context.props.loading,
+                    disabled: context.props.loading || context.props.disabled,
                 },
                 on: context.listeners,
             }, 'Save')
