93e0d6ef0018417931d38d8b0fe844aa9f00fa27
Antony King <antony.king@croud.co.uk>
2018-05-08 23:22:27 +0100

    add tests

--- test/unit/layout/toggle-header.spec.js
+++ test/unit/layout/toggle-header.spec.js
@@ -1,13 +1,36 @@
 
 import Vue from 'vue'
-import toggle-header from '../../../src/components/shared/layout/toggle-header'
+import toggleHeader from '../../../src/components/shared/layout/toggle-header'
 
-const Constructor = Vue.extend(toggle-header)
+const Constructor = Vue.extend(toggleHeader)
 
-const vm = new Constructor().$mount()
+const vm = new Constructor({
+    propsData: {
+        title: 'Test Header',
+        collapsed: false,
+    },
+}).$mount()
 
 describe('toggle-header', () => {
     it('should match the snapshot', () => {
         expect(vm.$el).toMatchSnapshot()
+    })
+
+    it('should show the collapse button', () => {
+        expect(vm.$el.querySelectorAll('.large.angle.down.icon').length).toBe(1)
+    })
+})
+
+describe('collapsed is false', () => {
+    beforeEach(() => {
+        vm.collapsed = true
+    })
+
+    it('should match the snapshot', () => {
+        expect(vm.$el).toMatchSnapshot()
+    })
+
+    it('should show the expand button', () => {
+        expect(vm.$el.querySelectorAll('.large.angle.right.icon').length).toBe(1)
     })
 })
