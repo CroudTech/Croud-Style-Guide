9db7764a35b0e6b01a9aa98870bba8901b948ac9
Brock <brock.reece@croud.co.uk>
2018-05-09 13:03:16 +0100

    Add arrow icon

---
+++ src/components/shared/icons/ArrowIcon.vue
@@ -0,0 +1,29 @@
+
+<script>
+    /**
+    * Standard arrow icon to use in Croud Control
+    *
+    * *This is a functional component which has no state so does not need to define any props*
+    * @see https://vuejs.org/v2/guide/render-function.html#Functional-Components
+    *
+    * @example ./croud-arrow-icon.md
+    */
+    export default {
+        functional: true,
+
+        name: 'croud-arrow-icon',
+
+        render(h, context) {
+            console.log(context)
+            return h('i', {
+                class: {
+                    chevron: true,
+                    icon: true,
+                    [context.props.direction || 'right']: true,
+                    ...context.data.class || {},
+                },
+            })
+        },
+    }
+</script>
+
