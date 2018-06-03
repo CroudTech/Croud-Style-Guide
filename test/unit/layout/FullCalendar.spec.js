55a1c4692109d68d61c2b78ca69c4637548e4740
Brock <brock.reece@croud.co.uk>
2018-06-03 12:19:39 +0100

    Increase test coverage

--- test/unit/layout/FullCalendar.spec.js
+++ test/unit/layout/FullCalendar.spec.js
@@ -1,10 +1,20 @@
 
 import Vue from 'vue'
+import VueFullCalendar from 'vue-full-calendar'
 import FullCalendar from '../../../src/components/shared/layout/FullCalendar'
+
+Vue.use(VueFullCalendar)
 
 const Constructor = Vue.extend(FullCalendar)
 
-const vm = new Constructor().$mount()
+const vm = new Constructor({
+    propsData: {
+        config: {
+            defaultDate: '2018-01-01',
+            defaultView: 'basicWeek',
+        },
+    },
+}).$mount()
 
 describe('FullCalendar', () => {
     it('should match the snapshot', () => {
@@ -30,10 +40,64 @@ describe('FullCalendar', () => {
             expect(vm.$el.querySelector('.ui.view.buttons')).toMatchSnapshot()
         })
 
+        it('highlights current view', (done) => {
+            vm.$nextTick(() => {
+                expect(vm.$el.querySelector('.blue.button')).toMatchSnapshot()
+                done()
+            })
+        })
+
         it('changes views based on props', (done) => {
             vm.views = [{ type: 'agendaWeek', title: 'Agenda' }, { type: 'basicWeek', title: 'Basic' }]
             vm.$nextTick(() => {
                 expect(vm.$el.querySelector('.ui.view.buttons')).toMatchSnapshot()
+                done()
+            })
+        })
+
+        it('updates calendar view', () => {
+            vm.changeView('month')
+            expect(vm.currentView).toBe('month')
+        })
+    })
+
+    describe('calendar controls', () => {
+        describe('steps date', () => {
+            it('forward', (done) => {
+                vm.step()
+
+                vm.$nextTick(() => {
+                    expect(vm.title).toBe('February 2018')
+                    done()
+                })
+            })
+
+            it('back', (done) => {
+                vm.step('prev')
+
+                vm.$nextTick(() => {
+                    expect(vm.title).toBe('January 2018')
+                    done()
+                })
+            })
+        })
+    })
+
+    describe('Calendar title and view', () => {
+        it('gets title when mounted', () => {
+            expect(vm.title).not.toBe('')
+        })
+
+        it('gets current view when mounted', () => {
+            expect(vm.currentView).not.toBe('')
+        })
+
+        it('updates title when view changes', (done) => {
+            vm.setTitle = jest.fn()
+            vm.step()
+
+            vm.$nextTick(() => {
+                expect(vm.setTitle).toBeCalled()
                 done()
             })
         })
