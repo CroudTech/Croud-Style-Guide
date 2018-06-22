a6f24f0375251158a835061643fa6973eb0c824d
Antony King <antony.king@croud.co.uk>
2018-06-22 14:43:23 +0100

    add test coverage / tweak to method

--- test/unit/misc/SummarisedMultiSelector.spec.js
+++ test/unit/misc/SummarisedMultiSelector.spec.js
@@ -130,4 +130,38 @@ describe('summarised multi selector', () => {
             })
         })
     })
+
+    describe('get array', () => {
+        it('should return an empty array if value is undefined', () => {
+            vm.value = undefined
+
+            const result = vm.getArray(vm.value)
+            const isArray = Array.isArray(result)
+
+            expect(isArray).toBe(true)
+            expect(result).toEqual([])
+        })
+
+        it('should return an empty array if value is an empty string', () => {
+            vm.value = ''
+
+            const result = vm.getArray(vm.value)
+            const isArray = Array.isArray(result)
+
+            expect(isArray).toBe(true)
+            expect(result).toEqual([])
+        })
+
+        it('should return an array of selected options', () => {
+            vm.value = 'foo,bar,baz'
+
+            const result = vm.getArray(vm.value)
+            const isArray = Array.isArray(result)
+
+            expect(isArray).toBe(true)
+            expect(result).toEqual([
+                'foo', 'bar', 'baz',
+            ])
+        })
+    })
 })
